c51e6863c49496608721aa0ed370e8f11408bf65
Kevin Rosenfield <kevin.rosenfield@gmail.com>
2023-09-25 11:08:35 -0400

    fourth commit

--- makeCars.go
+++ makeCars.go
@@ -4,6 +4,7 @@ import "fmt"
 
 type Car interface {
 	DescribeCar() string
+	GetMPG() float64
 }
 
 type sedan struct {
@@ -24,6 +25,10 @@ func (s sedan) DescribeCar() string {
 	return fmt.Sprintf("The sedan is %v, gets %v miles per gallon, and its transmission is %v.", s.color, s.mpg, s.transmission)
 }
 
+func (s sedan) GetMPG() float64 {
+	return s.mpg
+}
+
 type suv struct {
 	mpg          float64
 	color        string
@@ -42,6 +47,10 @@ func (u suv) DescribeCar() string {
 	return fmt.Sprintf("The %v, %v SUV gets %v miles per gallon.", u.color, u.transmission, u.mpg)
 }
 
+func (u suv) GetMPG() float64 {
+	return c.mpg
+}
+
 type coupe struct {
 	mpg          float64
 	color        string
@@ -58,4 +67,8 @@ func MakeCoupe(mpg float64, color string, transmission string) Car {
 
 func (c coupe) DescribeCar() string {
 	return fmt.Sprintf("The %v coupe is %v and gets %v miles per gallon.", c.transmission, c.color, c.mpg)
+}
+
+func (c coupe) GetMPG() float64 {
+	return c.mpg
 }
